44f0fa359d44e4e065a7e59ec0c0d02d26326596
Kirill <piksel-man231@yandex.ru>
2021-04-27 16:39:07 +0300

    Проверка работы 2х дисплеев

--- Home_METEO/Home_METEO.ino
+++ Home_METEO/Home_METEO.ino
@@ -1,13 +1,16 @@
 #include <QuadDisplay2.h>
 
-QuadDisplay dp1(9);
+QuadDisplay dp1(10);
+QuadDisplay dp2(9,5,7);
 
 void setup() {
   // put your setup code here, to run once:
   dp1.begin();
+  dp2.begin();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   dp1.displayInt(123);
+  dp2.displayInt(234);
 }
