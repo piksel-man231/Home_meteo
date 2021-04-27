3fd0a341badffe391cb11e7b91b829ed90567f52
Kirill <piksel-man231@yandex.ru>
2021-04-27 17:39:32 +0300

    Финальный Merge всех бревен

--- Home_METEO/Home_METEO.ino
+++ Home_METEO/Home_METEO.ino
@@ -1,16 +1,27 @@
+#include <TroykaDHT.h>
 #include <QuadDisplay2.h>
 
-QuadDisplay dp1(10);
+DHT sen(0, DHT11);
+QuadDisplay dp1(10,4,2);
 QuadDisplay dp2(9,5,7);
 
 void setup() {
   // put your setup code here, to run once:
   dp1.begin();
   dp2.begin();
+  Serial.begin(9600);
+  sen.begin();
 }
+
 
 void loop() {
   // put your main code here, to run repeatedly:
-  dp1.displayTemperatureC(24);
-  dp2.displayHumidity(60);
+  sen.read();
+  //Serial.print(sen.getHumidity());
+  int temp = 0;
+  temp = sen.getTemperatureC();
+  dp1.displayTemperatureC(temp);
+  int hum = 0;
+  hum = sen.getHumidity();
+  dp2.displayHumidity(hum);
 }
